df7f5c1b3d27364cc50653b4fb6444a979c51154
Ricardo Baumann <ricardo.baumann@zenvia.com.br>
2014-09-01 18:08:34 -0300

    mensagem

--- zf-issue.sh
+++ zf-issue.sh
@@ -3,21 +3,24 @@ verify() {
 	git show-ref --verify --quiet refs/heads/develop
 
 	if [ $? -eq 1 ] ; then
+		echo "tilt"
 		git checkout -B develop
+	else
+		echo "not tilt"
 	fi	
 }
 
 
 start() {
-	verify()
-	echo "Issue branch $1 will be created"
+	verify
 	git fetch && git checkout develop
 	git checkout -B $1
 }
 
 push() {
-	verify()
-	git add -u . && git commit -m $3 && git push origin $2
+	verify
+	git add .
+	git commit -m $1
 }
 
 pull() {
